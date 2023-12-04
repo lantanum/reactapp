689f242687eb4c02cd236e59a61801aeb7ce4ce3
Akbar <geraldrosentalie@gmail.com>
2023-12-05 00:34:27 +0600

    Шаг восьмой

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import SwapiService from "../../services/swapi-service";
 import Spinner from '../spinner'
+import ErrorIndicator from "../error-indicator";
 
 import './random-planet.css';
 
@@ -10,7 +11,8 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
     
 
@@ -28,13 +30,26 @@ export default class RandomPlanet extends Component {
 
     updatePlanet() {
         const id = Math.floor(Math.random() * 25 + 2);
-        this.swapiService.getPlanet(id).then(this.onPlanetLoaded)
+        this.swapiService.getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    };
+
     render() {
-        const { planet, loading} = this.state;
+        const { planet, loading, error} = this.state;
+
+        const hasData = !(loading || error);
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={planet} /> : null;
+        const content = hasData ? <PlanetView planet={planet} /> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
@@ -58,6 +73,7 @@ export default class RandomPlanet extends Component {
                         </li>
                     </ul>
                 </div>*/}
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
