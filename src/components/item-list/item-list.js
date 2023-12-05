4a18df2ff8648794b389653e9241ad287c7933bd
Akbar <geraldrosentalie@gmail.com>
2023-12-05 13:57:47 +0600

    Шаг одиннадцатый

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -21,12 +21,9 @@ export default class ItemList extends Component {
             })
     }
 
-    renderItems = (arr) => {
+    renderItems(arr) {
         return arr.map((item) => {
             const {id} = item;
-
-            if(!this.props.renderItem) debugger;
-
             const label = this.props.renderItem(item);
             return (
                 <li
